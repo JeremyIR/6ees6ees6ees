eccaecce4edc5f545fa7d8455e6f1dae10c3468a
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-20 15:05:49 -0700

    Create HoneyMakerBee subclass of Bee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,7 +1,25 @@
-import Bee from './Bee'
+import Bee from './Bee';
 
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
   // TODO..
-};
+ constructor (age, color) {
+  super(age, color);
+  this.age = 10;
+  this.color = 'yellow';
+  this.job = 'make honey';
+  this.honeyPot = 0;
+ }
+
+  eat() {
+   eat.super();
+  }
+
+ makeHoney(honeyPot) {
+  return this.honeyPot++;
+ }
+ giveHoney(honeyPot) {
+  return this.honeyPot--;
+ }
+}
 
 export default HoneyMakerBee;
