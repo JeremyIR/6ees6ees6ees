206decf7322e3701764b12ea93febfc4f310ec4c
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-20 15:06:17 -0700

    Create ForagerBee subclass of Bee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,7 +1,23 @@
-import Bee from './Bee'
+import Bee from './Bee';
 
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
-};
+ constructor (age, color) {
+  super(age, color);
+  this.age = 10;
+  this.color = 'yellow';
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+ }
+
+ eat() {
+  eat.super();
+ }
+
+ forage(treasure) {
+  this.treasureChest.push(treasure);
+ }
+}
 
 export default ForagerBee;
