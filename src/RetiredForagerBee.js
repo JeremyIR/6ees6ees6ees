8e46d74e787a2e233748eb9fbec7927a8f4c0a51
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-20 15:06:45 -0700

    Create RetiredForagerBee subclass of Forager Bee

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,7 +1,27 @@
 import ForagerBee from './ForagerBee';
 
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
   // TODO..
-};
+ constructor (age, color, treasureChest) {
+  super(age, color, treasureChest);
+  this.age = 40;
+  this.color = 'grey';
+  this.job = 'gamble';
+  this.canFly = false;
+ }
+
+ eat() {
+  eat.super();
+ }
+
+ forage(treasure) {
+  return 'I am too old, let me play cards instead';
+ }
+
+ gamble(treasure) {
+  this.treasureChest.push(treasure);
+ }
+}
 
 export default RetiredForagerBee;
+
